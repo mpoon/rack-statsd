a0e0b12b4e1493475bf04f74463b80aff05b2a78
mpoon <mpoon@twitch.tv>
2013-08-05 13:52:58 -0700

    removed the env delete

--- lib/rack-statsd.rb
+++ lib/rack-statsd.rb
@@ -333,9 +333,6 @@ module RackStatsD
       env['process.request_start'] = @start.to_f
       env['process.total_requests'] = total_requests
 
-      # newrelic X-Request-Start
-      env.delete('HTTP_X_REQUEST_START')
-
       status, headers, body = @app.call(env)
       body = Body.new(body) { record_request(status, env) }
       [status, headers, body]
